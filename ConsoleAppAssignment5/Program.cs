7cd885cf87ef643f8f4fce2cb868ab7d26b13f90
prakash1926 <ananthaprakash71@gmail.com>
2023-11-09 16:34:06 +0530

    Assignment5.1

---
+++ ConsoleAppAssignment5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name :");
+            string String = Console.ReadLine();
+
+            Console.WriteLine("Enter your Age :");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your Height :");
+            double Height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nName :" + String + "\nAge :" + age + "\nHeight :" + Height);
+
+            Console.ReadKey();
+        }
+    }
+}
